e3fe9f0a6d881f17e5c7b543940111e0eea3544d
Hercules Agent <noreply@usehercules.com>
2026-03-31 15:42:02 +0000

    Manual checkpoint 1774971722161

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -57,7 +57,6 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="bg-inherit fill-inherit z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-[2px]" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   );
